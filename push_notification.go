4a026c173968a5fb885926b2173539fd934fc5c2
Alex Stokes <alex@meetgo.me>
2015-03-17 13:56:59 -0700

    Update payload with custom field

--- push_notification.go
+++ push_notification.go
@@ -45,6 +45,7 @@ type Payload struct {
 	Sound            string      `json:"sound,omitempty"`
 	ContentAvailable int         `json:"content-available,omitempty"`
 	Category         string      `json:"category,omitempty"`
+	Title            string      `json:"title,omitempty"`
 }
 
 // NewPayload creates and returns a Payload structure.
